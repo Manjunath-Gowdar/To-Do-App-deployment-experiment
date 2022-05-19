0e19b17da99ae41dda4703f4047dced3e0c31e72
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-05-19 23:28:25 +0530

    frontend user functionality update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,44 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../actions/userAction'
 
 const Header = () => {
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  const dispatch = useDispatch()
+
+  const logoutHandler = (e) => {
+    e.preventDefault()
+    dispatch(logout())
+  }
+
   return (
     <header>
       <nav>
-           <Link to='/home' style={{'paddingRight':'20px'}}>HOME</Link>
-           <Link to='/login' style={{'paddingRight':'20px'}}>LOGIN</Link>
-           <Link to='/register' style={{'paddingRight':'20px'}}>REGISTER</Link>
+        <Link to='/home' style={{ paddingRight: '20px' }}>
+          HOME
+        </Link>
+        {userInfo ? (
+          <>
+            {/* <Link to='/profile'>Profile</Link> */}
+
+            <button style={{ marginLeft: '20px' }} onClick={logoutHandler}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to='/login' style={{ paddingRight: '20px' }}>
+              LOGIN
+            </Link>
+
+            <Link to='/register' style={{ paddingRight: '20px' }}>
+              REGISTER
+            </Link>
+          </>
+        )}
       </nav>
     </header>
   )
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,68 +1,11 @@
-// import React, { useState, useEffect } from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { login } from '../actions/userAction'
-
-// const LoginScreen = ({ location, history }) => {
-//   const [email, setEmail] = useState('')
-//   const [password, setPassword] = useState('')
-
-//   const dispatch = useDispatch()
-
-//   const userLogin = useSelector((state) => state.userLogin)
-//   const { loading, error, userInfo } = userLogin
-
-//   const redirect = location.search ? location.search.split('=')[1] : '/'
-
-//   useEffect(() => {
-//     if (userInfo) {
-//       history.push(redirect)
-//     }
-//   }, [history, userInfo, redirect])
-
-//   const submitHandler = (e) => {
-//     e.preventDefault()
-//     dispatch(login(email, password))
-//   }
-
-//   return (
-//     <div>
-
-//         <h1>hello login</h1>
-//       {/* <h1>Sign In</h1>
-//       {error && <h3>{error}</h3>}
-//       {loading && <h2>LOADING</h2>}
-//        <form>
-//             <label for='email'>Enter your Email</label>
-//             <input type='text' id='email' name='email'/>
-            
-//             <label for='password'>Enter your password</label>
-//             <input type='text' id='password' name='password'/>
-            
-//             <input type='submit' value='submit' onSubmit={submitHandler}/>
-//        </form> */}
-
-//       {/* <Row className='py-3'>
-//         <Col>
-//           New Customer?{' '}
-//           <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
-//             Register
-//           </Link>
-//         </Col>
-//       </Row> */}
-//     </div>
-//   )
-// }
-
-// export default LoginScreen
-
-import React,{useState,useEffect} from 'react'
-import { useLocation , useNavigate} from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { login } from '../actions/userAction'
 
 const LoginScreen = () => {
-const navigate  = useNavigate()
-    const [email, setEmail] = useState('')
+  const navigate = useNavigate()
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   const dispatch = useDispatch()
@@ -70,12 +13,9 @@ const navigate  = useNavigate()
   const userLogin = useSelector((state) => state.userLogin)
   const { loading, error, userInfo } = userLogin
 
-//   console.log(location)
-//   const redirect = location.search ? location.search.split('=')[1] : '/'
-  
   useEffect(() => {
     if (userInfo) {
-        navigate('/')
+      navigate('/')
     }
   }, [navigate, userInfo])
 
@@ -86,18 +26,32 @@ const navigate  = useNavigate()
   }
   return (
     <div>
-       <h1>Sign In</h1>
-       {error && <h3>{error}</h3>}
-       {loading && <h2>LOADING</h2>}
-        <form onSubmit={submitHandler}>
-             <label for='email'>Enter your Email</label><br/>
-             <input type='text' id='email' name='email' onChange={e => setEmail(e.target.value)}/><br />
-            
-             <label for='password'>Enter your password</label><br/>
-             <input type='text' id='password' name='password' onChange={e=>setPassword(e.target.value)}/><br/>
-            
-             <input type='submit' value='submit' />
-        </form>
+      <h1>Sign In</h1>
+      {error && <h3>{error}</h3>}
+      {loading && <h2>LOADING..</h2>}
+      <form onSubmit={submitHandler}>
+        <label for='email'>Enter your Email</label>
+        <br />
+        <input
+          type='text'
+          id='email'
+          name='email'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+
+        <label for='password'>Enter your password</label>
+        <br />
+        <input
+          type='text'
+          id='password'
+          name='password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+
+        <input type='submit' value='submit' />
+      </form>
     </div>
   )
 }
