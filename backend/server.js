4981b8580461c4e4be77955816ece448561f46d5
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-05-24 02:49:31 +0530

    Prepared App for Deployment

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,21 @@ connectDB()
 const app = express()
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('api is running')
-})
-
 app.use('/api/todos', todoRoutes)
 app.use('/api/users', userRoutes)
+
+// required code for deployment, to select build as static folder
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
