ec445f50741b9dbf4db4484abe95ebd4ebd73420
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-05-24 11:01:26 +0530

    Readme.md update

---
+++ frontend/asset/futureUpdate.js
@@ -0,0 +1,6 @@
+// To give priority to todo with star logo 
+// to make the app look good on all screen size 
+// custome error handling
+// coding proper CRUD request
+// to add user profile screen with user icon on headers 
+
